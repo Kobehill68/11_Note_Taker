d6027f7cbd050294dab5c009a8d42515af0de0fc
KobeHill68 <kobehill68@gmail.com>
2020-11-22 14:25:20 -0600

    set up my server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes");
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("./public"));
+
+
+app.use(routes);
+
+
+
+app.listen(PORT, () => {
+    console.log("App listening on PORT: " + PORT);
+});
