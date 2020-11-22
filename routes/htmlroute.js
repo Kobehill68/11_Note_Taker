f9c788bbe5b99f5c0da4a69c1ca791d17f37bfb9
KobeHill68 <kobehill68@gmail.com>
2020-11-22 14:26:37 -0600

    added my routes file

---
+++ routes/htmlroute.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const router = require("express").Router();
+
+router.get('/notes', (_req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+router.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+router.get("*", (_req, res) => {
+    res.sendFile(__dirname, "../public/index.html")
+})
+
+
+
+module.exports = router;
